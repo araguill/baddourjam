488d0904ee5858cb1b017fbb6b756f218a2e8d85
araguill <guillermo.suarez@gmail.com>
2018-12-05 10:29:20 -0500

    nick

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import nick from './nick.png'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={nick} className="App-logo" alt="logo" />
           <p>
             Can Matt Baddour come out and jam?
           </p>
