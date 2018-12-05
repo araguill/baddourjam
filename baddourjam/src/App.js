8098cfa387743b5b53fded8aab0b108b6ad4e369
araguill <guillermo.suarez@gmail.com>
2018-12-05 10:17:19 -0500

    stuff
    stuff

---
+++ baddourjam/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Can Matt Baddour come out and jam?
+          </p>
+          <a
+            className="App-link"
+            href="https://sudoroso.bandcamp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            "We're working on it"
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
