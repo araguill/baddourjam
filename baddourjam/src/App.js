153f2972e65ca98b4916ac7fee22b7ea724c06c3
araguill <guillermo.suarez@gmail.com>
2018-12-05 10:19:36 -0500

    text

--- baddourjam/src/App.js
+++ baddourjam/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            "We're working on it"
+            Find out
           </a>
         </header>
       </div>
